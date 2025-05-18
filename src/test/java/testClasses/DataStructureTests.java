cba74b494c6b82aebc4410ff95b738db375a30ed
PramodiniPatil1 <pramodini.patil182@gmail>
2025-05-18 14:48:39 -0400

    latest changes to main

--- src/test/java/testClasses/DataStructureTests.java
+++ src/test/java/testClasses/DataStructureTests.java
@@ -1,78 +1,80 @@
 package testClasses;
 
 import java.io.IOException;
-import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.*;
+import baseClass.BaseClass;
 import driverManager.DriverFactory;
-import dsAlgoPageObjects.DataStructurePageObj;
-import dsAlgoPageObjects.HomePageObj;
-import dsAlgoPageObjects.RegisterPageObj;
-import dsAlgoPageObjects.SignInPageObj;
-import dsAlgoPageObjects.TryEditorPage;
-import io.cucumber.core.internal.com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import dsAlgoPageObjects.*;
 import utils.ConfigReader;
+import utils.DataProviders;
 
-public class DataStructureTests {
 
-    static WebDriver driver;
-    static DataStructurePageObj dataStructurerpage;
-    static TryEditorPage tryEditorPage;
-    static HomePageObj homepage;
-    static RegisterPageObj registerpage;
-    static SignInPageObj signinpage;
+public class DataStructureTests extends BaseClass {
 
-    @BeforeClass
-    public void setUp() throws InvalidFormatException, IOException, OpenXML4JException {
-        String browser = ConfigReader.getBrowserType();
-        driver = DriverFactory.initializeDriver(browser);
+    WebDriver driver;
+    DataStructurePageObj dataStructurerpage;
+    TryEditorPage tryEditorPage;
+    HomePageObj homepage;
+    RegisterPageObj registerpage;
+    SignInPageObj signinpage;
 
-        // Initialize page objects
+    @BeforeMethod
+    public void setUpDataStructure() throws IOException {
+        driver = DriverFactory.initializeDriver(ConfigReader.getBrowserType());
+        registerpage = new RegisterPageObj(driver);
         dataStructurerpage = new DataStructurePageObj(driver);
         tryEditorPage = new TryEditorPage(driver);
         homepage = new HomePageObj(driver);
         registerpage = new RegisterPageObj(driver);
-        signinpage =new SignInPageObj(driver);
+        signinpage = new SignInPageObj(driver);
 
-        // Navigate and login
         driver.get(ConfigReader.getUrl());
         homepage.clickGetStartedHomePageButton();
-       
         homepage.clickSignInLink();
         signinpage.EnterFromExcel("login", 0);
         signinpage.clickloginButton();
-        Assert.assertEquals(registerpage.successMsg(), "You are logged in");
-    }
 
-    @Test
-    public void testValidOutputforTimeComplexity() throws Exception {
-    	 homepage.clickDsGetStartedButton();
-        dataStructurerpage.ClickTimeComplexityLink();
-        tryEditorPage.clickTryHereButton();
-        tryEditorPage.enterCodeFromExcel("tryEditorCode", 0);
-        tryEditorPage.clickRunButton();
-        Assert.assertEquals(tryEditorPage.getOutputText(), "Python is fun!");
-    }
-
-    @Test
-    public void testInValidOutputforTimeComplexity() throws Exception {
-    	 homepage.clickDsGetStartedButton();
-        dataStructurerpage.ClickTimeComplexityLink();
-        tryEditorPage.clickTryHereButton();
-        tryEditorPage.enterCodeFromExcel("tryEditorCode", 1);
-        tryEditorPage.clickRunButton();
-
-        String actualAlertText = tryEditorPage.HandleAlert();
-        System.out.println("Actual Alert Text: " + actualAlertText);
-
-        String expectedAlertText = "SyntaxError: bad input on line 1";
-        Assert.assertTrue(actualAlertText.contains(expectedAlertText),
-            "Expected alert to contain: " + expectedAlertText  + actualAlertText);
+        Assert.assertEquals(registerpage.successMsg(), "You are logged in", "Login failed or success message mismatch.");
+   
+        driver.get(ConfigReader.getUrl());
+		homepage.clickGetStartedHomePageButton();
         
     }
 
+
+    @Test(priority = 1, dataProvider = "ValidPythonCode", dataProviderClass = DataProviders.class)
+    
   
+    public void testValidOutputforTimeComplexity1(String sheetName, int rowNum, String expectedOutput) throws Exception {
+        homepage.clickDsGetStartedButton();
+        dataStructurerpage.ClickTimeComplexityLink();
+        tryEditorPage.clickTryHereButton();
+        tryEditorPage.enterCodeFromExcel(sheetName, rowNum);
+        tryEditorPage.clickRunButton();
+
+        Assert.assertEquals(tryEditorPage.getOutputText(), expectedOutput, "Valid output did not match expected result.");
+    }
+
+    @Test(priority = 1, dataProvider = "InvalidPythonCode", dataProviderClass = DataProviders.class)
+    public void testInValidOutputforTimeComplexity(String sheetName, int rowNum, String expectedAlertPart) throws Exception {
+        homepage.clickDsGetStartedButton();
+        dataStructurerpage.ClickTimeComplexityLink();
+        tryEditorPage.clickTryHereButton();
+        tryEditorPage.enterCodeFromExcel(sheetName, rowNum);
+        tryEditorPage.clickRunButton();
+
+        String alertMessage = tryEditorPage.getAlertText(); // Make sure this returns the actual alert text
+        Assert.assertNotNull(alertMessage, "Expected alert was not present!");
+        Assert.assertTrue(alertMessage.contains(expectedAlertPart),
+            "Alert message did not contain expected text. Actual: " + alertMessage);
+
+    }
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.closeDriver();  // This calls quit() and also removes the ThreadLocal
+    
+    }
 }
--- src/test/java/testClasses/RegisterTests.java
+++ src/test/java/testClasses/RegisterTests.java
@@ -1,86 +1,91 @@
 package testClasses;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import baseClass.BaseClass;
 import driverManager.DriverFactory;
-
 import dsAlgoPageObjects.RegisterPageObj;
 import utils.ConfigReader;
 
 public class RegisterTests extends BaseClass {
-	RegisterPageObj registerpage;
+    RegisterPageObj registerpage;
 
-	@BeforeMethod
-	public void setUpRegister() {
-		WebDriver driver;
-		driver = DriverFactory.initializeDriver(ConfigReader.getBrowserType());
-		registerpage = new RegisterPageObj(driver);
-		driver.get(ConfigReader.getUrl());
-		registerpage.clickGetStartedButton();
-		registerpage.clickRegisterLink();
-	}
+    @BeforeMethod
+    public void setUpRegister() {
+        driver = DriverFactory.initializeDriver(ConfigReader.getBrowserType());
+        registerpage = new RegisterPageObj(driver);
+        driver.get(ConfigReader.getUrl());
+        registerpage.clickGetStartedButton();
+        registerpage.clickRegisterLink();
+    }
 
-	@DataProvider(name = "validRegisterData")
-	public Object[][] validData() {
-		return new Object[][] { { "Register", 4 } };
-	}
+    @DataProvider(name = "validRegisterData")
+    public Object[][] validData() {
+        return new Object[][] {
+            {"Register", 4}
+        };
+    }
 
-	@DataProvider(name = "passwordMismatchData")
-	public Object[][] mismatchData() {
-		return new Object[][] { { "Register", 1 } };
-	}
+    @DataProvider(name = "passwordMismatchData")
+    public Object[][] mismatchData() {
+        return new Object[][] {
+            {"Register", 1}
+        };
+    }
 
-	@DataProvider(name = "emptyUsernameData")
-	public Object[][] emptyData() {
-		return new Object[][] { { "Register", 0 } };
-	}
+    @DataProvider(name = "emptyUsernameData")
+    public Object[][] emptyData() {
+        return new Object[][] {
+            {"Register", 0}
+        };
+    }
 
-	@DataProvider(name = "numericPasswordData")
-	public Object[][] numericPasswordData() {
-		return new Object[][] { { "Register", 2 } };
-	}
+    @DataProvider(name = "numericPasswordData")
+    public Object[][] numericPasswordData() {
+        return new Object[][] {
+            {"Register", 2}
+        };
+    }
 
-	@Test(dataProvider = "validRegisterData")
-	public void testValidRegistration(String sheetName, int row) throws Exception {
-		registerpage.fillRegistrationForm(sheetName, row);
-		registerpage.clickRegisterButton();
+    @Test(dataProvider = "validRegisterData")
+    public void testValidRegistration(String sheetName, int row) throws Exception {
+        registerpage.fillRegistrationForm(sheetName, row);
+        registerpage.clickRegisterButton();
 
-		Assert.assertTrue(registerpage.checkIfRegisterSuccessMsgIsDisplayed(), "Success message not displayed");
-		System.out.println("Success message: " + registerpage.successMsg());
-	}
+        Assert.assertTrue(registerpage.checkIfRegisterSuccessMsgIsDisplayed(), "Success message not displayed");
+        System.out.println("Success message: " + registerpage.successMsg());
+    }
 
-	@Test(dataProvider = "passwordMismatchData")
-	public void testPasswordMismatchRegistration(String sheetName, int row) throws Exception {
-		registerpage.fillRegistrationForm(sheetName, row);
-		registerpage.clickRegisterButton();
+    @Test(dataProvider = "passwordMismatchData")
+    public void testPasswordMismatchRegistration(String sheetName, int row) throws Exception {
+        registerpage.fillRegistrationForm(sheetName, row);
+        registerpage.clickRegisterButton();
 
-		Assert.assertTrue(registerpage.isPasswordMismatchVisible(), "Mismatch error not displayed");
-		System.out.println("Password mismatch message: " + registerpage.getPasswordMismatchText());
-	}
+        Assert.assertTrue(registerpage.isPasswordMismatchVisible(), "Mismatch error not displayed");
+        System.out.println("Password mismatch message: " + registerpage.getPasswordMismatchText());
+    }
 
-	@Test(dataProvider = "emptyUsernameData")
-	public void testEmptyUsernameValidation(String sheetName, int row) throws Exception {
-		registerpage.fillRegistrationForm(sheetName, row);
-		registerpage.clickRegisterButton();
+    @Test(dataProvider = "emptyUsernameData")
+    public void testEmptyUsernameValidation(String sheetName, int row) throws Exception {
+        registerpage.fillRegistrationForm(sheetName, row);
+        registerpage.clickRegisterButton();
 
-		String msg = registerpage.switchToElementAndGetValidationMessage();
-		System.out.println("Browser validation message: " + msg);
-		Assert.assertTrue(msg.contains("fill out this field"), "Expected browser validation not displayed");
-	}
+        String msg = registerpage.switchToElementAndGetValidationMessage();
+        System.out.println("Browser validation message: " + msg);
+        Assert.assertTrue(msg.contains("fill out this field"), "Expected browser validation not displayed");
+    }
 
-	@Test(dataProvider = "numericPasswordData")
-	public void testNumericPasswordValidation(String sheetName, int row) throws Exception {
-		registerpage.fillRegistrationForm(sheetName, row);
-		registerpage.clickRegisterButton();
+    @Test(dataProvider = "numericPasswordData")
+    public void testNumericPasswordValidation(String sheetName, int row) throws Exception {
+        registerpage.fillRegistrationForm(sheetName, row);
+        registerpage.clickRegisterButton();
 
-		Assert.assertTrue(registerpage.isPasswordMismatchVisible(), "Mismatch error not displayed");
-		System.out.println("Password mismatch message: " + registerpage.getPasswordMismatchText());
-	}
+        Assert.assertTrue(registerpage.isPasswordMismatchVisible(), "Mismatch error not displayed");
+        System.out.println("Password mismatch message: " + registerpage.getPasswordMismatchText());
+    }
 
-	@AfterMethod
-	public void tearDown() {
-		DriverFactory.closeDriver();
-	}
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.closeDriver();
+    }
 }
